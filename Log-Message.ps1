006e211cdee0102e71bf6e6e1d74871ec019a801
Asbjørn Vang Jacobsen <51018443+avjacobsen@users.noreply.github.com>
2021-09-25 23:46:42 +0100

    Added Box parameter
    Setting box parameter to $true will paint a box og asterisk around the message.

--- Log-Message.ps1
+++ Log-Message.ps1
@@ -10,7 +10,10 @@ function Write-LogMessage {
         $MessageType = "INFO",
         [Parameter(Mandatory = $false)]
         [String]
-        $Path = ""
+        $Path = "",
+        [Parameter(Mandatory = $false)]
+        [Boolean]
+        $Box = $false
     )
     if ($Path -eq "" -and $PSCommandPath -ne "") {
         # No path supplied but running from script. Setting path to script name.
@@ -20,10 +23,27 @@ function Write-LogMessage {
         # No path supplied and not running from script. Logging to file skipped.
     }
     $MessagePrefix = "$(Get-Date -Format "yyyy").$(Get-Date -Format "MM").$(Get-Date -Format "dd") $(Get-Date -Format "HH"):$(Get-Date -Format "mm"):$(Get-Date -Format "ss") "
+    $BoxedMessage = "* $Message *"
+    for ($i = 0; $i -le ($BoxedMessage.Length - 1); $i++) { $BoxBar += '*' }
     if ($Path -ne "") {
-        Add-Content -Path $Path -Value "$($MessagePrefix)[$($MessageType)] $($Message)"
+        if ($Box) {
+            Add-Content -Path $Path -Value "$($MessagePrefix)[$($MessageType)] $($BoxBar)"
+            Add-Content -Path $Path -Value "$($MessagePrefix)[$($MessageType)] $($BoxedMessage)"
+            Add-Content -Path $Path -Value "$($MessagePrefix)[$($MessageType)] $($BoxBar)"
+        }
+        else {
+            Add-Content -Path $Path -Value "$($MessagePrefix)[$($MessageType)] $($Message)"
+        }
     }
     if ($VerbosePreference -or $DebugPreference) {
-        Write-Host "$($MessagePrefix)[$($MessageType)] $($Message)"
+        if ($Box) {
+            Write-Host "$($MessagePrefix)[$($MessageType)] $($BoxBar)"
+            Write-Host "$($MessagePrefix)[$($MessageType)] $($BoxedMessage)"
+            Write-Host "$($MessagePrefix)[$($MessageType)] $($BoxBar)"
+        }
+        else {
+            Write-Host "$($MessagePrefix)[$($MessageType)] $($Message)"
+        }
     }
 }
+
